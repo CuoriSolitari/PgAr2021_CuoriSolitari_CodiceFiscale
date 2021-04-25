79b1b6f89de4a26b5f41479b006abbab4a3f074c
paoloformentelli <p.formentelli001@studenti.unibs.it>
2021-04-25 16:46:21 +0200

    Prova3

--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Ciao");
         System.out.println("ciao lino");
+        System.out.println("ciao Giuliano");
 
     }
 }
