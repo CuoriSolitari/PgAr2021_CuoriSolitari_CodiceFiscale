e1bd4db947965ccd78d276be25bc9bb259526e67
paoloformentelli <p.formentelli001@studenti.unibs.it>
2021-04-29 17:30:51 +0200

    Classi2

---
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/CodiceFiscale.java
@@ -0,0 +1,11 @@
+package it.unibs.prgarnaldo.cuorisolitari.codicefiscale;
+
+public class CodiceFiscale {
+
+    private boolean valido=false;
+
+    public boolean verificaCF(String codiceFiscale){
+
+        return valido;
+    }
+}
--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/CreaCF.java
+++
@@ -1,5 +0,0 @@
-package it.unibs.prgarnaldo.cuorisolitari.codicefiscale;
-
-public class CreaCF {
-
-}
