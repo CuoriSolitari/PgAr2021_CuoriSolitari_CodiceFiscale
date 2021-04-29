9f7b09f9ef82a60eff535f9358d463183008be38
paoloformentelli <p.formentelli001@studenti.unibs.it>
2021-04-29 18:43:24 +0200

    Prova metodo

--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/CodiceFiscale.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/CodiceFiscale.java
@@ -7,16 +7,27 @@ public class CodiceFiscale {
     private char sesso;
     private Data data;
     private String luogo;
-    private char carCtrl;
+    private char car_ctrl;
     private String codice_fiscale;
-    private boolean valido=false;
 
-    public CodiceFiscale(String nome, String cognome, char sesso, Data data, String luogo, char carCtrl) {
+
+    public CodiceFiscale(String nome, String cognome, char sesso, Data data, String luogo, char car_ctrl) {
 
     }
 
-    public boolean verificaCF(String codiceFiscale){
+    /**
+     * Questo metodo ha una stringa in ingresso e restituisce vero nel caso il codice fiscale sia valido, falso se non  lo è
+     *
+     * @param codice_fiscale
+     * @return
+     */
+    public static boolean verificaCF(String codice_fiscale){
 
-        return valido;
+        //Se la stringa contine meno di 16 caratteri si ferma subito perchè non può essere un codice fiscale
+        if (16 != codice_fiscale.length()){
+            return false;
+        }
+
+        return true;
     }
 }
--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
@@ -3,11 +3,9 @@ package it.unibs.prgarnaldo.cuorisolitari.codicefiscale;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.out.println("Ciao");
-        System.out.println("ciao lino");
-        System.out.println("ciao Giuliano");
-        System.out.println("vica è un gay");
-        System.out.println("vica svegliati che altrimenti a fine serata siamo ancora qua");;
+        String prova = "aaa";
 
+        boolean a = CodiceFiscale.verificaCF(prova);
+        if (a==false) System.out.println("ciao");
     }
 }
