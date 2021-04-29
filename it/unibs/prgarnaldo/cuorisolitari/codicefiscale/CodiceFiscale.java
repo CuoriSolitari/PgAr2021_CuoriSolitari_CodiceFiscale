237dac5909b4d3e794b6f91e27361c4addd5c964
paoloformentelli <p.formentelli001@studenti.unibs.it>
2021-04-29 19:25:47 +0200

    metodoCF

--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/CodiceFiscale.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/CodiceFiscale.java
@@ -16,16 +16,31 @@ public class CodiceFiscale {
     }
 
     /**
-     * Questo metodo ha una stringa in ingresso e restituisce vero nel caso il codice fiscale sia valido, falso se non  lo è
+     * Questo metodo ha una stringa in ingresso e restituisce vero nel caso il codice fiscale sia valido, falso se non lo è
      *
-     * @param codice_fiscale
+     * @param codice
      * @return
      */
-    public static boolean verificaCF(String codice_fiscale){
+    public static boolean verificaCF(String codice){
 
         //Se la stringa contine meno di 16 caratteri si ferma subito perchè non può essere un codice fiscale
-        if (16 != codice_fiscale.length()){
+        if (16 != codice.length()){
             return false;
+        }
+        //Ciclo for che controlla carattere per carattere la stringa codice fiscale
+        for(int i=0; i<16; i++){
+            //Controlla tutti le posizioni dei caratteri
+            if(i>=0 && i<=5 || i==8 || i==11 || i==15){
+                //Se non è un carattere maiuscolo secondo la tabella ASCII restituisce falso
+                if(codice.charAt(i)<65 || codice.charAt(i)>90)
+                    return false;
+            }
+            //Controlla tutti le posizioni dei numeri
+            else{
+                //Se non è un numero secondo la tabella ASCII restituisce falso
+                if(codice.charAt(i)<48 || codice.charAt(i)>57)
+                    return false;
+            }
         }
 
         return true;
--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
@@ -3,9 +3,10 @@ package it.unibs.prgarnaldo.cuorisolitari.codicefiscale;
 public class MainClass {
 
     public static void main(String[] args) {
-        String prova = "aaa";
+        String prova = "FRMPLA01D26D434N";
 
         boolean a = CodiceFiscale.verificaCF(prova);
         if (a==false) System.out.println("ciao");
+        else System.out.println("bella");
     }
 }
