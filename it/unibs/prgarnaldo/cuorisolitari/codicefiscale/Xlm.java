a18bcf399b54a3e782e4ee0d6df22b25f240449b
Giucsy <giulironchi201336@gmail.com>
2021-05-01 12:58:08 +0200

    xml

--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/Xlm.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/Xlm.java
@@ -1,27 +1,72 @@
 package it.unibs.prgarnaldo.cuorisolitari.codicefiscale;
 
+
 import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamWriter;
-import java.io.FileInputStream;;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-public class Xlm {
+public class  Xlm {
 
-    XMLInputFactory xmlif = null;
-    XMLStreamReader xmlr = null;
+    public static Xlm Readfile() throws FileNotFoundException, XMLStreamException {
+
+        XMLInputFactory xmlif = null;
+        XMLStreamReader xmlr = null;
+        try {
+            xmlif = XMLInputFactory.newInstance();
+            xmlr = xmlif.createXMLStreamReader("esempio.xml", new FileInputStream("esempio.xml"));
+            while (xmlr.hasNext())
+            { // continua a leggere finché ha eventi a disposizione
+                switch (xmlr.getEventType())
+                { // switch sul tipo di evento
+                    case XMLStreamConstants.START_DOCUMENT: // inizio del documento: stampa che inizia il documento
+                        System.out.println("Start Read Doc " + "esempio.xml"); break;
+                    case XMLStreamConstants.START_ELEMENT: // inizio di un elemento: stampa il nome del tag e i suoi attributi
+                        System.out.println("Tag " + xmlr.getLocalName());
+                        for (int i = 0; i < xmlr.getAttributeCount(); i++)
+                            System.out.printf(" => attributo %s->%s%n", xmlr.getAttributeLocalName(i), xmlr.getAttributeValue(i));
+                        break;
+                    case XMLStreamConstants.END_ELEMENT: // fine di un elemento: stampa il nome del tag chiuso
+                        System.out.println("END-Tag " + xmlr.getLocalName()); break;
+                    case XMLStreamConstants.COMMENT:
+                        System.out.println("// commento " + xmlr.getText()); break; // commento: ne stampa il contenuto
+                    case XMLStreamConstants.CHARACTERS: // content all’interno di un elemento: stampa il testo
+                        if (xmlr.getText().trim().length() > 0) // controlla se il testo non contiene solo spazi
+                            System.out.println("-> " + xmlr.getText());
+                        break;
+                }
+                xmlr.next();
+            }
+
+        }
+        xmlr.close();
 
 
-    try{
-        xmlif = XMLInputFactory.newInstance();
-        xmlr = xmlif.createFilteredReader(codiciFiscali, new FileInputStream(codiciFiscali.xml));
+        } catch (Exception e)
+        {
+            System.out.println("Errore nell'inizializzazione del reader:");
+            System.out.println(e.getMessage());
+        }
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
     }
 
-    catch( Exception e)
-    {
-        System.out.println("Errore nell'inizializzazione del reader:");
-        System.out.println(e.getMessage());
-    }
 
 
 }
