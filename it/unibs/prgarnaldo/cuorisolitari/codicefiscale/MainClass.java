a99858e18d59747ecd5653d4df19629754bde345
paoloformentelli <p.formentelli001@studenti.unibs.it>
2021-05-03 12:44:28 +0200

    writer

--- it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
+++ it/unibs/prgarnaldo/cuorisolitari/codicefiscale/MainClass.java
@@ -2,7 +2,10 @@ package it.unibs.prgarnaldo.cuorisolitari.codicefiscale;
 
 import UniBSFpLib.src.it.unibs.fp.mylib.InputDati;
 
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
@@ -33,7 +36,28 @@ public class MainClass {
                     valido = CodiceFiscale.verificaCF(codice);
                     System.out.println(codice + " " + valido);
                 }
-
+            }
+            //Comando 3: genera l'XML
+            else if(comando == 3){
+                File file = new File("it/unibs/prgarnaldo/cuorisolitari/codicefiscale/codiciPersone.xml");
+                XMLOutputFactory xmlof = null;
+                XMLStreamWriter xmlw = null;
+                try {
+                    xmlw.writeStartElement(String.valueOf(file));
+                    xmlw.writeComment("INIZIO LISTA");
+                    /*for (int i = 0; i < autori.length; i++) {
+                        xmlw.writeStartElement("autore");
+                        xmlw.writeAttribute("id", Integer.toString(i));
+                        xmlw.writeCharacters(autori[i]);
+                        xmlw.writeEndElement();
+                    }*/
+                    xmlw.writeEndElement();
+                    xmlw.writeEndDocument();
+                    xmlw.flush();
+                    xmlw.close();
+                } catch (Exception e) {
+                    System.out.println("Errore nella scrittura");
+                }
             }
 
         } while(comando != 4);
